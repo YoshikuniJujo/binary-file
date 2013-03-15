4d6e5997249f8b617c2490f7b850fc22d245d3a0
iocikun.jujon <PAF01143@nifty.ne.jp>
2013-03-15 14:09:05 +0900

    tribial change

--- test.hs
+++ test.hs
@@ -2,6 +2,7 @@
 
 import QuoteBinaryStructure
 import System.Environment
+import Data.Bits
 
 [binary|
 
@@ -15,6 +16,29 @@ BitmapFileHeader
 4: 40
 4: bitmapWidth
 4: bitmapHeight
+2: 1
+2: bitPerPic
+4: compress
+4: imageDataSize
+4: horizontalDensity
+4: verticalDensity
+4: colorIndexNumber
+4: neededIndexNumber
+4: color1
+4: color2
+4: color3
+4: color4
+4: color5
+4: color6
+4: color7
+4: color8
+4: color9
+4: color10
+4: color11
+4: color12
+4: color13
+40: color1423
+4: color24
 
 |]
 
@@ -22,3 +46,10 @@ main = do
 	[fn] <- getArgs
 	cnt <- readFile fn
 	print $ readBitmapFileHeader cnt
+
+toRGB :: Int -> (Int, Int, Int)
+toRGB rgb = let
+	b = rgb .&. 0xff
+	g = shiftR rgb 8 .&. 0xff
+	r = shiftR rgb 16 in
+	(r, g, b)
