b0aa7770eff822aaf8abcc09475b34c8091b4f2f
iocikun.jujon <PAF01143@nifty.ne.jp>
2013-03-27 11:41:52 +0900

    wellPNG.hs OK

--- wellPNG.hs
+++ wellPNG.hs
@@ -1,18 +1,19 @@
-{-# LANGUAGE QuasiQuotes, TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes, TypeFamilies, FlexibleInstances #-}
 
 import File.Binary
 import File.Binary.Data.BigEndian
 import System.Environment
 import Data.Word
+import qualified Data.ByteString as BS
 
 main = do
 	[f] <- getArgs
 	cnt <- readBinaryFile f
-	let (png, rest) = readPNG cnt
+	let (png, rest) = readPNG () cnt
 	print $ png
 	print $ map chankName $ chanks png
 
-test = readPNG `fmap` readBinaryFile "tmp/out.png"
+test = readPNG () `fmap` readBinaryFile "tmp/out.png"
 
 instance RetType Word32 where
 	type Argument Word32 = Int
@@ -21,8 +22,17 @@ instance RetType Word32 where
 
 instance RetType Chank where
 	type Argument Chank = ()
-	fromType _ = writeChank
-	toType _ = readChank
+	fromType = writeChank
+	toType = readChank
+
+instance RetType (Int, Int, Int) where
+	type Argument (Int, Int, Int) = ()
+	fromType _ (b, g, r) = cc [fromType 1 b, fromType 1 g, fromType 1 r]
+	toType _ s = let
+		(b, rest) = toType 1 s
+		(g, rest') = toType 1 rest
+		(r, rest'') = toType 1 rest' in
+		((b, g, r), rest'')
 
 [binary|
 
@@ -42,24 +52,39 @@ data ChankBody
 	| ChankGAMA GAMA
 	| ChankSRGB SRGB
 	| ChankCHRM CHRM
+	| ChankPLTE PLTE
+	| ChankBKGD BKGD
+	| ChankIDAT IDAT
+	| ChankTEXT TEXT
+	| ChankIEND IEND
 	| Others String
 	deriving Show
 
 instance RetType ChankBody where
 	type Argument ChankBody = (Int, String)
-	fromType _ (ChankIHDR ihdr) = writeIHDR ihdr
-	fromType _ (ChankGAMA gama) = writeGAMA gama
-	fromType _ (ChankSRGB srgb) = writeSRGB srgb
-	fromType _ (ChankCHRM chrm) = writeCHRM chrm
+	fromType _ (ChankIHDR ihdr) = writeIHDR () ihdr
+	fromType _ (ChankGAMA gama) = writeGAMA () gama
+	fromType _ (ChankSRGB srgb) = writeSRGB () srgb
+	fromType _ (ChankCHRM chrm) = writeCHRM () chrm
 	fromType (n, _) (Others str) = fromType ((), Just n) str
-	toType (_, "IHDR") str = let (ihdr, rest) = readIHDR str in
+	toType (_, "IHDR") str = let (ihdr, rest) = readIHDR () str in
 		(ChankIHDR ihdr, rest)
-	toType (_, "gAMA") str = let (gama, rest) = readGAMA str in
+	toType (_, "gAMA") str = let (gama, rest) = readGAMA () str in
 		(ChankGAMA gama, rest)
-	toType (_, "sRGB") str = let (srgb, rest) = readSRGB str in
+	toType (_, "sRGB") str = let (srgb, rest) = readSRGB () str in
 		(ChankSRGB srgb, rest)
-	toType (_, "cHRM") str = let (chrm, rest) = readCHRM str in
+	toType (_, "cHRM") str = let (chrm, rest) = readCHRM () str in
 		(ChankCHRM chrm, rest)
+	toType (n, "PLTE") str = let (plte, rest) = readPLTE n str in
+		(ChankPLTE plte, rest)
+	toType (_, "bKGD") str = let (bkgd, rest) = readBKGD () str in
+		(ChankBKGD bkgd, rest)
+	toType (n, "IDAT") str = let (idat, rest) = readIDAT n str in
+		(ChankIDAT idat, rest)
+	toType (n, "tEXt") str = let (text, rest) = readTEXT n str in
+		(ChankTEXT text, rest)
+	toType (_, "IEND") str = let (iend, rest) = readIEND () str in
+		(ChankIEND iend, rest)
 	toType (n, _) str = let (others, rest) = toType ((), Just n) str in
 		(Others others, rest)
 
@@ -126,6 +151,40 @@ PLTE
 
 <Int>
 
--- ((), arg `div` 3)<[(Int, Int, Int)]>: colors
+((), Just (arg `div` 3))<[(Int, Int, Int)]>: colors
+
+|]
+
+[binary|
+
+BKGD
+
+1: bkgd
+
+|]
+
+[binary|
+
+IDAT
+
+<Int>
+
+arg<BS.ByteString>: idat
+
+|]
+
+[binary|
+
+TEXT
+
+<Int>
+
+((), Just arg)<String>: text
+
+|]
+
+[binary|
+
+IEND
 
 |]
