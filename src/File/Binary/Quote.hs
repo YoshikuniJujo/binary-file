244acb4e75bfb2fe368f8a622b8675bc50d07011
iocikun.jujon <PAF01143@nifty.ne.jp>
2013-04-04 18:01:18 +0900

    refactoring: Quote.hs mkDef

--- src/File/Binary/Quote.hs
+++ src/File/Binary/Quote.hs
@@ -5,7 +5,7 @@ module File.Binary.Quote (Field(..), Binary(..), binary) where
 import Language.Haskell.TH (
 	Q, Name, newName, varP, tupP, integerL, stringL,
 	ExpQ, varE, litE, appE, appsE, infixE, tupE, letE, condE, sigE, listE, recConE,
-	TypeQ, appT, conT,
+	TypeQ, appT, conT, infixApp,
 	DecsQ, DecQ, Dec, valD, dataD, funD, instanceD, tySynInstD,
 	normalB, cxt, clause, recC, varStrictType, strictType, notStrict)
 import Language.Haskell.TH.Quote (QuasiQuoter(..))
@@ -57,7 +57,7 @@ reading name bsn argn body = do
 	arg <- newName "_arg"
 	bin <- newName "bin"
 	funD name [clause [varP arg, varP bin]
-		(normalB $ mkLetRec $ mkBody bsn (varE arg) argn body bin) []]
+		(normalB $ mkLetRec $ mkBody bsn (varE arg) argn body (varE bin)) []]
 
 mkLetRec :: (ExpQ -> ExpQ) -> ExpQ
 mkLetRec e = do
@@ -65,54 +65,53 @@ mkLetRec e = do
 	letE [valD (tupP [varP ret, varP rest]) (normalB $ e $ varE ret) []] $
 		tupE [varE ret, varE rest]
 
-mkBody :: Name -> ExpQ -> Name -> [BSItem] -> Name -> ExpQ -> ExpQ
+mkBody :: Name -> ExpQ -> Name -> [BSItem] -> ExpQ -> ExpQ -> ExpQ
 mkBody bsn arg argn body bin ret = do
-	namePairs <- for names $ \n -> return . (n ,) =<< newName "tmp"
-	(defs, rest) <- mapM (mkDef getN namePairs) body `runStateT` bin
-	letE (map return defs) $ tupE
-		[recConE bsn (map toPair namePairs), varE rest]
-	where
-	names = map fst $ variables body
-	toPair (n, nn) = (n, ) <$> varE nn
-	getN = expression ret arg argn . bytesOf
+	nps <- for (variables body) $ \(n, _) -> (n ,) <$> newName "tmp"
+	(defs, rest) <- (`runStateT` bin) $
+		for body $ mkDef nps $ expression ret arg argn . bytesOf
+	letE (return <$> defs) $ tupE $ (: [rest]) $ recConE bsn $ (<$> nps) $
+		\(n, tmp) -> (n ,) <$> varE tmp
 
-mkDef :: (BSItem -> ExpQ) -> [(Name, Name)] -> BSItem -> StateT Name Q Dec
-mkDef getN np item
-    | Constant (Left val) <- valueOf item = do
-	cs' <- get
-	cs'' <- lift $ newName "cs"
-	let	t = dropE' (getN item) $ varE cs'
-		p = val `equal` appE (varE 'fst)
-			(appE (appE (varE 'fromBinary) $ getN item) $
-				takeE' (getN item) $ varE cs')
-		e = [e| error "bad value" |]
-	d <- lift $ valD (varP cs'') (normalB $ condE p t e) []
-	put cs''
-	return d
+mkDef :: [(Name, Name)] -> (BSItem -> ExpQ) -> BSItem -> StateT ExpQ Q Dec
+mkDef np getn item
+	| Constant (Left val) <- valueOf item = do
+		bin <- get
+		fv <- lift $ newName "fv"
+		ret <- lift $ newName "rt"
+		bin' <- lift $ newName "bin'"
+		put $ varE bin'
+		lift $ flip (valD $ varP bin') [] $ normalB $
+			letE [flip (valD $ tupP [varP fv, varP ret]) [] $ normalB $
+					appsE [varE 'fromBinary, getn item, bin]] $
+			condE (infixApp (varE fv) (varE '(==))
+					(litE (integerL val) `sigE` conT ''Integer))
+				(varE ret)
+				[e| error "bad value" |]
     | Constant (Right val) <- valueOf item = do
 	cs' <- get
-	cs'' <- lift $ newName "cs"
-	let t = dropE' (getN item) $ varE cs'
-	let p = val `equal'` takeE' (getN item) (varE cs')
+	cs'' <- lift $ newName "bin'"
+	let t = dropE' (getn item) cs'
+	let p = val `equal'` takeE' (getn item) cs'
 	let e = [e| error "bad value" |]
 	d <- lift $ valD (varP cs'') (normalB $ condE p t e) []
-	put cs''
+	put $ varE cs''
 	return d
     | Variable var <- valueOf item = do
 	cs' <- get
-	cs'' <- lift $ newName "cs"
+	cs'' <- lift $ newName "bin'"
 	def <- lift $ valD (tupP [varP $ fromJust $ lookup var np, varP cs''])
-		(normalB $ appE (appE (varE 'fromBinary) $ getN item) $ varE cs') []
-	put cs''
+		(normalB $ appE (appE (varE 'fromBinary) $ getn item) cs') []
+	put $ varE cs''
 	return def
-    | otherwise = error "bad"
+    | otherwise = error "never occur"
 
 writing :: Name -> Name -> [BSItem] -> DecQ
 writing name argn body = do
 	arg <- newName "_arg"
 	bs <- newName "_bs"
-	let run = appE (varE 'mconcat) $ listE $ map
-		(\bsi -> writeField bs arg argn (bytesOf bsi) (valueOf bsi)) body
+	let run = appE (varE 'mconcat) $ listE $ (<$> body) $
+		writeField bs arg argn <$> bytesOf <*> valueOf
 	funD name
 		[clause [varP arg, varP bs] (normalB run) []]
 
@@ -141,15 +140,11 @@ fieldValueToStr bs arg argn size True = \val ->
 getField :: Name -> Name -> ExpQ
 getField bs v = appE (varE v) (varE bs)
 
-equal :: Integer -> ExpQ -> ExpQ
-equal x y = infixE (Just $ sigE (litE $ integerL x) (conT ''Integer))
-	(varE '(==)) (Just y)
-
 equal' :: String -> ExpQ -> ExpQ
 equal' x y = infixE (Just $ litE $ stringL x) (varE '(==)) (Just y)
 
 takeE' :: ExpQ -> ExpQ -> ExpQ
-takeE' n xs = -- appE (varE 'ts) $ appsE [varE 'tk, n, xs]
+takeE' n xs =
 	appE (varE 'BSLC.unpack) $ appE (varE 'fst) $ appsE [varE 'getBytes, n, xs]
 
 dropE' :: ExpQ -> ExpQ -> ExpQ
