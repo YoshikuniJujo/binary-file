f5bdf9bcf659a5ce1d0658f9ffcf0794dcd5cd9b
iocikun.jujon <PAF01143@nifty.ne.jp>
2013-04-10 17:25:51 +0900

    refactoring: examples/CRC.hs

--- examples/CRC.hs
+++ examples/CRC.hs
@@ -1,4 +1,4 @@
-module CRC (crc) where
+module CRC (crc, testCRC) where
 
 import Data.Array
 import Data.Bits
@@ -6,23 +6,24 @@ import Data.Char
 import Data.Word
 import Numeric
 
+--------------------------------------------------------------------------------
+
 crc :: String -> Word32
 crc = xor 0xffffffff . foldl crc' 0xffffffff
 	where
 	crc' :: Word32 -> Char -> Word32
-	crc' c x =
-		let i = xor c (fromIntegral $ ord x) .&. 0xff
-		 in xor (table ! i) $ shiftR c 8
+	crc' c x = table ! i `xor` shiftR c 8
+		where
+		i = (c `xor` fromIntegral (ord x)) .&. 0xff
 	table :: Array Word32 Word32
 	table = listArray (0, 255) $ map (\n -> foldl table' n [0 .. 7]) [0 .. 255]
-		where
-		table' :: Word32 -> Int -> Word32
-		table' c _
-			| c .&. 1 == 0 = shiftR c 1
-			| otherwise = xor 0xedb88320 $ shiftR c 1
+	table' :: Word32 -> Int -> Word32
+	table' c _
+		| c .&. 1 == 0 = shiftR c 1
+		| otherwise = xor 0xedb88320 $ shiftR c 1
 
-main :: IO ()
-main = p . crc =<< getContents
+testCRC :: IO ()
+testCRC = p . crc =<< getContents
 	where
 	p :: Word32 -> IO ()
 	p c = putStrLn $ "0x" ++ showHex c ""
